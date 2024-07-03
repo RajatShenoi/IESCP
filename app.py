292bf68fe0130bf3264a8405296d991a0cdcad2e
Rajat Shenoi <rajatshenoi@outlook.com>
2024-07-03 18:21:05 +0530

    basic homepage

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run()
